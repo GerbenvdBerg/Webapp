ceb2613a542bfc1636b2ee5e61f83b5a8fd3ac40
GerbenvdBerg <gerbenberg06@gmail.com>
2025-05-23 13:05:34 +0200

    Main van website
    Main

---
+++ main.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Welkom</title>
+</head>
+<body>
+    <h1><?php echo "Hallo op de website van Gerben"; ?></h1>
+</body>
+</html>
